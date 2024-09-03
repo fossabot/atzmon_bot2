fc887a0df558ac2b615998a847f9d6b31cae3cf4
Asaf Atzmon <asaf.atzmon@gmail.com>
2024-09-03 12:56:02 +0300

    chore: Update main.js and .gitignore files

--- main.js
+++ main.js
@@ -21,14 +21,13 @@ try {
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
-    authStrategy: new LocalAuth(),
+    authStrategy: new LocalAuth({
+            clientId: process.env.CLIENT_ID,
+        }
+    ),
     puppeteer: {
         headless: true,
         args: ['--no-sandbox'],
-    },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
     },
 });
 
---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set database connection parameters
+export DB_HOST="localhost"
+export DB_USER="aatzmon"
+export DB_PASSWORD="morissey\$2Smith"
+export DB_DATABASE="aatzmon_db"
+export OPENAI_API_KEY=""
+export CLIENT_ID="local"
+
+# Print the values to verify
+echo "DB_HOST=$DB_HOST"
+echo "DB_USER=$DB_USER"
+echo "DB_PASSWORD=$DB_PASSWORD"
+echo "DB_DATABASE=$DB_DATABASE"
